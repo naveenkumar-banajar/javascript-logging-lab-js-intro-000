4b152a99a63a2668fff1e80465522f4e7beb6892
Naveen kumar <naveenkumar.banajar@gmail.com>
2017-09-08 11:12:39 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -8,13 +8,13 @@ describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
-  it(console.error("HALP!"), done => {
+  it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
-      expect(spy).toHaveBeenCalled('expected console.error to have been called')
+      console.error('expected console.error to have been called')
       console.error.restore()
       done()
     })
